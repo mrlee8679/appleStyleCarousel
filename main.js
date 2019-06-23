d99accbb083cd5cea4ec71a11574fba54b7815e4
mrlee8679 <bleach111314@gmail.com>
2019-06-23 13:22:01 +0800

    所有图片从在线图片改为本地图片，修改js代码

--- main.js
+++ main.js
@@ -2,67 +2,58 @@ let $lis = $('ul>li')
 let $slides = $('#slides')
 let $images = $slides.children('img')
 let current = 0
-let $content = $('#slidesWrapper').width()
-let $button = $('li>a')
 
 makeFakeSlides()
 bindEvents()
-$slides.css({transform:`translateX(${-($content)}px)`})
+$slides.css({ transform: 'translateX(-920px)' })
 $lis.eq(0).addClass('active')
 
-let timer = setInterval(function(){
-  if(current +1 >3){
-    $lis.removeClass('active')
-    $lis.eq(0).addClass('active')
-  }else{
-    $lis.removeClass('active')
-    $lis.eq(current+1).addClass('active')
-  }
-  
-  goToSlide(current+1)
-},3000)
+let timer = setInterval(function () {
+  addActive(current)
+  goToSlide(current + 1)
+}, 3000)
 
-$('.container').on('mouseenter',function(){
+$('.container').on('mouseenter', function () {
   window.clearInterval(timer)
-}).on('mouseleave',function(){
-  timer = setInterval(function(){
-    if(current +1 >3){
-      $lis.removeClass('active')
-      $lis.eq(0).addClass('active')
-    }else{
-      $lis.removeClass('active')
-      $lis.eq(current+1).addClass('active')
-    }
-    goToSlide(current+1)
-  },3000)
+}).on('mouseleave', function () {
+  timer = setInterval(function () {
+    addActive(current)
+    goToSlide(current + 1)
+  }, 3000)
 })
 
-document.addEventListener('visibilitychange',function(e){
-  if(document.hidden){
+document.addEventListener('visibilitychange', function (e) {
+  if (document.hidden) {
     window.clearInterval(timer)
-  }else{
-    timer = setInterval(function(){
-      if(current +1 >3){
-        $lis.removeClass('active')
-        $lis.eq(0).addClass('active')
-      }else{
-        $lis.removeClass('active')
-        $lis.eq(current+1).addClass('active')
-      }
-      goToSlide(current+1)
-    },3000)
+  } else {
+    timer = setInterval(function () {
+      addActive(current)
+      goToSlide(current + 1)
+    }, 3000)
   }
 })
 
-function makeFakeSlides(){
+
+
+function makeFakeSlides() {
   let $firstCopy = $images.eq(0).clone(true)
-  let $lastCopy = $images.eq($images.length-1).clone(true)
+  let $lastCopy = $images.eq($images.length - 1).clone(true)
   $slides.append($firstCopy)
   $slides.prepend($lastCopy)
 }
 
-function bindEvents(){
-  $('ul').on('click','li',function(e){
+function addActive() {
+  if (current + 1 > 3) {
+    $lis.removeClass('active')
+    $lis.eq(0).addClass('active')
+  } else {
+    $lis.removeClass('active')
+    $lis.eq(current + 1).addClass('active')
+  }
+}
+
+function bindEvents() {
+  $('ul').on('click', 'li', function (e) {
     let $li = $(e.currentTarget)
     let index = $li.index()
     $lis.removeClass('active')
@@ -71,30 +62,26 @@ function bindEvents(){
   })
 }
 
-
-function goToSlide (index){
-  if (index > $lis.length-1){
+function goToSlide(index) {
+  if (index > $lis.length - 1) {
     index = 0
-  }else if (index<0){
-    index.$lis.length-1
+  } else if (index < 0) {
+    index = $lis.length - 1
   }
-  $lis.removeClass('active')
-  $lis.eq(index).addClass('active')
-  if (current === $lis.length-1 && index === 0){
-    $slides.css({transform:`translateX(${-($lis.length+1) * 920}px)`})
-      .one('transitionend',function(){
+  if (current === $lis.length - 1 && index === 0) {
+    $slides.css({ transform: `translateX(${-($lis.length + 1) * 920}px)` })
+      .one('transitionend', function () {
         $slides.hide().offset()
-        $slides.css({transform:`translateX(${-(index+1) * 920}px)`}).show()
+        $slides.css({ transform: `translateX(${-(index + 1) * 920}px)` }).show()
       })
-  }else if (current ===0 && index === $lis.length-1){
-    $slides.css({transform:'translateX(0px)'})
-      .one('transitionend',function(){
+  } else if (current === 0 && index === $lis.length - 1) {
+    $slides.css({ transform: 'translateX(0px)' })
+      .one('transitionend', function () {
         $slides.hide().offset()
-        $slides.css({transform:`translateX(${-(index+1) * 920}px)`}).show()
-    })
-  }else{
-    $slides.css({transform:`translateX(${-(index+1) * 920}px)`})
+        $slides.css({ transform: `translateX(${-(index + 1) * 920}px)` }).show()
+      })
+  } else {
+    $slides.css({ transform: `translateX(${-(index + 1) * 920}px)` })
   }
   current = index
-  
 }
