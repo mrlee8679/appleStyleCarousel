0089e8acf0e91b524b44bf8383c23675cb3f6848
mrlee8679 <bleach111314@gmail.com>
2019-06-23 11:42:48 +0800

    完成了苹果风格的轮播，可以自动轮播，也快点击轮播

--- main.js
+++ main.js
@@ -1,9 +1,58 @@
+let $lis = $('ul>li')
 let $slides = $('#slides')
 let $images = $slides.children('img')
 let current = 0
+let $content = $('#slidesWrapper').width()
+let $button = $('li>a')
 
 makeFakeSlides()
-$slides.css({transform:'translateX(-720px)'})
+bindEvents()
+$slides.css({transform:`translateX(${-($content)}px)`})
+$lis.eq(0).addClass('active')
+
+let timer = setInterval(function(){
+  if(current +1 >3){
+    $lis.removeClass('active')
+    $lis.eq(0).addClass('active')
+  }else{
+    $lis.removeClass('active')
+    $lis.eq(current+1).addClass('active')
+  }
+  
+  goToSlide(current+1)
+},3000)
+
+$('.container').on('mouseenter',function(){
+  window.clearInterval(timer)
+}).on('mouseleave',function(){
+  timer = setInterval(function(){
+    if(current +1 >3){
+      $lis.removeClass('active')
+      $lis.eq(0).addClass('active')
+    }else{
+      $lis.removeClass('active')
+      $lis.eq(current+1).addClass('active')
+    }
+    goToSlide(current+1)
+  },3000)
+})
+
+document.addEventListener('visibilitychange',function(e){
+  if(document.hidden){
+    window.clearInterval(timer)
+  }else{
+    timer = setInterval(function(){
+      if(current +1 >3){
+        $lis.removeClass('active')
+        $lis.eq(0).addClass('active')
+      }else{
+        $lis.removeClass('active')
+        $lis.eq(current+1).addClass('active')
+      }
+      goToSlide(current+1)
+    },3000)
+  }
+})
 
 function makeFakeSlides(){
   let $firstCopy = $images.eq(0).clone(true)
@@ -13,7 +62,39 @@ function makeFakeSlides(){
 }
 
 function bindEvents(){
-  if(current === $buttons.length-1 && index === 0 ){
-    $slides.css({transform:`translateX(${-(-720px)})`})
+  $('ul').on('click','li',function(e){
+    let $li = $(e.currentTarget)
+    let index = $li.index()
+    $lis.removeClass('active')
+    $lis.eq(index).addClass('active')
+    goToSlide(index)
+  })
+}
+
+
+function goToSlide (index){
+  if (index > $lis.length-1){
+    index = 0
+  }else if (index<0){
+    index.$lis.length-1
   }
+  console.log(index)
+  if (current === $lis.length-1 && index === 0){
+    
+    $slides.css({transform:`translateX(${-($lis.length+1) * 920}px)`})
+    
+      .one('transitionend',function(){
+        $slides.hide().offset()
+        $slides.css({transform:`translateX(${-(index+1) * 920}px)`}).show()
+      })
+  }else if (current ===0 && index === $lis.length-1){
+    $slides.css({transform:'translateX(0px)'})
+      .one('transitionend',function(){
+        $slides.hide().offset()
+        $slides.css({transform:`translateX(${-(index+1) * 920}px)`}).show()
+    })
+  }else{
+    $slides.css({transform:`translateX(${-(index+1) * 920}px)`})
+  }
+  current = index
 }
